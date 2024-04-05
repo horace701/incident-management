c0fb85a277e0a227e6e903c51bd7b79020f2777d
Horace Yau <horace701@hotmail.com>
2024-04-05 18:11:07 -0400

    Update vite.config.js and index.html

--- client/vite.config.js
+++ client/vite.config.js
@@ -18,11 +18,15 @@ export default defineConfig({
             },
         },
     },
-
+/*
     build: {
         manifest: true,
         rollupOptions: {
             input: "./src/main.jsx",
         },
     },
+*/    
+    build: {
+        outDir: '../dist/app',
+    }
 });
